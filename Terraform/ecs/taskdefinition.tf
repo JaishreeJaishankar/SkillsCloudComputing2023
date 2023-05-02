c44953a75b8946a0e0cc72989263e664f035cf92
Jaishree Jaishankar <jaishree.jaishankar@riseup.net>
2023-05-02 17:44:55 +0000

    Change in task definition

--- Terraform/ecs/taskdefinition.tf
+++ Terraform/ecs/taskdefinition.tf
@@ -4,8 +4,8 @@ resource "aws_ecs_task_definition" "this" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws
-  task_role_arn            = var.iam_role
+  execution_role_arn       = aws_iam_role.this.arn
+  task_role_arn            = aws_iam_role.this.arn
   container_definitions = jsonencode([{
     name      = "this-container"
     image     = "379114597277.dkr.ecr.us-east-1.amazonaws.com/this-repo:latest"
